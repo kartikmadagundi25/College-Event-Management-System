b35aef8b7582397b314a817b4dc3e8691cdfea85
Kartik Madagundi <148052008+kartikmadagundi25@users.noreply.github.com>
2024-07-20 19:27:32 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,6 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <h6 style="text-align:center;"> Copyright by :Kartik Madagundi </h6>
+    </div>
+</footer>
